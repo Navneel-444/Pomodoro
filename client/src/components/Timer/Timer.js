9f22e6aba2d6338be6d4ea187546addc9987eb5e
Navneel Nandran <navneel.nandran@outlook.com>
2025-02-11 22:33:05 -0500

    feat: records appear after work session is complete

--- client/src/components/Timer/Timer.js
+++ client/src/components/Timer/Timer.js
@@ -9,15 +9,17 @@ export default function Timer() {
     const radius = 92;
     const circumference = 2 * Math.PI * radius;
     const trackerRecord = document.getElementsByClassName("timer-tracker__marker")
-
-
     // States 
     const [presetTime, setPresetTime] = useState(5);
-    // const [presetRest, setPresetRest] = useState(5);
+    const [presetRest, setPresetRest] = useState(2);
+    const [presetLongRest, setPresetLongRest] = useState(4);
+
     const [elapsed, setElapsed] = useState(presetTime);
     const [elapsedCircle, setElapsedCircle] = useState(circumference);
     const [active, setActive] = useState(false);
     const [tracker, setTracker] = useState(0);
+    const [activeSession, setActiveSession] = useState(true);
+
     // Clock
     function formatTime(elapsed) {
         const minutes = Math.floor(elapsed / 60);
@@ -31,14 +33,13 @@ export default function Timer() {
         });
     }
 
-    function completeMarker(numCompleted) {
+    function marker(numCompleted) {
         if (numCompleted === 4) {
-            removeMarker();
+            removeMarker()
             setTracker(0)
-        } else {
-            const completedMarker = trackerRecord[numCompleted]
-            completedMarker.classList.add("timer-tracker__marker--completed")
         }
+        const completedMarker = trackerRecord[numCompleted]
+        completedMarker.classList.add("timer-tracker__marker--completed")
     }
     // Controls
     function handlePlay() {
@@ -46,12 +47,20 @@ export default function Timer() {
     }
 
     function handleRestart() {
-        setElapsed(presetTime);
-        setElapsedCircle(presetTime * circumference);
+        setActive(false)
+        const rest = tracker === 3 ? presetLongRest : presetRest
+        const time = activeSession ? rest : presetTime
+        setElapsed(time);
+        setElapsedCircle(time * circumference);
+    }
+
+    function handleSkip() {
+        handleRestart()
+        setActiveSession(activeSession ? false : true)
     }
 
     useEffect(() => {
-        if (active === true) {
+        if (active & elapsed >= 0) {
             const timer = setInterval(() => {
                 setElapsed((prevTime) => {
                     if (prevTime === 0) {
@@ -65,12 +74,20 @@ export default function Timer() {
                 });
             }, 1000)
             if (elapsed === 0) {
-                setTracker((prevNum) => prevNum + 1)
-                completeMarker(tracker)
+                setActive(false);
+                if (activeSession) {
+                    setActiveSession(false);
+                    setTracker((prevNum) => prevNum + 1);
+                    marker(tracker);
+                    handleRestart();
+                } else {
+                    handleRestart();
+                    setActiveSession(true);
+                }
             }
             return () => clearInterval(timer);
         }
-    }, [circumference, active, elapsedCircle, elapsed]);
+    }, [circumference, active, elapsedCircle, elapsed, tracker]);
 
     return (
         <>
@@ -81,7 +98,7 @@ export default function Timer() {
                     </span>
                     <svg className="timer-clock__svg" xmlns="http://www.w3.org/2000/svg">
                         <circle className="timer-clock__path-elapsed" cx="50%" cy="50%" r={radius}></circle>
-                        <circle className="timer-clock__path-remaining" cx="50%" cy="50%" r={radius} strokeDasharray={`${elapsedCircle} ${circumference}`}></circle>
+                        <circle className="timer-clock__path-remaining" cx="50%" cy="50%" r={radius} stroke={activeSession ? "#01497C" : "#7e3501"} strokeDasharray={`${elapsedCircle} ${circumference}`}></circle>
                     </svg>
                     <section className="timer-tracker">
                         <svg className="timer-tracker__record" >
@@ -104,9 +121,9 @@ export default function Timer() {
                     <img className="timer-controls__image" src={restart} alt="Restarts the current timer" />
                 </button>
                 <button className="timer-controls__buttons" onClick={handlePlay}>
-                    <img className="timer-controls__image" src={active ? pause : play} alt="Pauses the current timer" />
+                    <img className="timer-controls__image" src={active & elapsed > 0 ? pause : play} alt="Pauses the current timer" />
                 </button>
-                <button className="timer-controls__buttons">
+                <button className="timer-controls__buttons" onClick={handleSkip}>
                     <img className="timer-controls__image" src={skip} alt="Skips the session" />
                 </button>
             </section>
